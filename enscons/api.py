48b6a7c7cc0510362f6b54daa030de08ce90752c
Christoph Wiedemann <62332054+cwiede@users.noreply.github.com>
2023-08-03 16:51:20 +0200

    implement suggestion in https://github.com/dholth/enscons/issues/36

--- enscons/api.py
+++ enscons/api.py
@@ -32,20 +32,28 @@ def _run(alias):
 
 def prepare_metadata_for_build_wheel(metadata_directory, settings):
     sys.argv[1:] = ["--wheel-dir=" + metadata_directory, "dist_info"]
+    for k in settings:
+        sys.argv.append(f"{k}={settings[k]}")
     return _run("dist_info")
 
 
 def build_wheel(wheel_directory, settings, metadata_directory=None):
     sys.argv[1:] = ["--wheel-dir=" + wheel_directory, "bdist_wheel"]
+    for k in settings:
+        sys.argv.append(f"{k}={settings[k]}")
     return _run("bdist_wheel")
 
 
 def build_sdist(sdist_directory, settings):
     sys.argv[1:] = ["--dist-dir=" + sdist_directory, "sdist"]
+    for k in settings:
+        sys.argv.append(f"{k}={settings[k]}")
     return _run("sdist")
 
 
 # PEP 660 editable installation
 def build_editable(wheel_directory, config_settings=None, metadata_directory=None):
     sys.argv[1:] = ["--wheel-dir=" + wheel_directory, "editable"]
+    for k in config_settings:
+        sys.argv.append(f"{k}={config_settings[k]}")
     return _run("editable")
